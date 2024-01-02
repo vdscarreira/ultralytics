662f6ced7ca276e4a3fbc1a426ec246e996cb5ed
vdscarreira <102616180+vdscarreira@users.noreply.github.com>
2024-01-02 19:28:23 -0300

    Add files via upload

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -195,6 +195,7 @@ class C2f(nn.Module):
         self.c = int(c2 * e)  # hidden channels
         self.cv1 = Conv(c1, 2 * self.c, 1, 1)
         self.cv2 = Conv((2 + n) * self.c, c2, 1)  # optional act=FReLU(c2)
+        #self.m = nn.ModuleList(GhostBottleneck(self.c, self.c, k = 3, s = 1) for _ in range(n))
         self.m = nn.ModuleList(Bottleneck(self.c, self.c, shortcut, g, k=((3, 3), (3, 3)), e=1.0) for _ in range(n))
 
     def forward(self, x):
@@ -208,8 +209,7 @@ class C2f(nn.Module):
         y = list(self.cv1(x).split((self.c, self.c), 1))
         y.extend(m(y[-1]) for m in self.m)
         return self.cv2(torch.cat(y, 1))
-
-
+        
 class C3(nn.Module):
     """CSP Bottleneck with 3 convolutions."""
 
